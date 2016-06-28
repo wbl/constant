2feb881b5df898e06c37294726b9de40e73ff712
Watson Ladd <wladd@mozilla.com>
2016-06-28 11:11:37 -0700

    Very close to working. Need to add double scalarmult and tests, and then integrate

--- p384/p384_32.c
+++ p384/p384_32.c
@@ -5,7 +5,6 @@
 
 #include<stdint.h>
 #include<stdbool.h>
-#include<assert.h>
 #include<stdio.h>
 #include<string.h>
 #include "p384_32.h"
@@ -64,7 +63,42 @@ static const uint32_t R[12]={0x1,
 			     0x0,
                              0x0};
 
-
+static const uint32_t curve_b[12]={0xd3ec2aef,
+				   0x2a85c8ed,
+				   0x8a2ed19d,
+				   0xc656398d,
+				   0x5013875a,
+				   0x314088f,
+				   0xfe814112,
+				   0x181d9c6e,
+				   0xe3f82d19,
+				   0x988e056b,
+				   0xe23ee7e4,
+				   0xb3312fa7};
+static const uint32_t base_x[12] ={0x72760ab7,
+				   0x3a545e38,
+				   0xbf55296c,
+				   0x5502f25d,
+				   0x82542a38,
+				   0x59f741e0,
+				   0x8ba79b98,
+				   0x6e1d3b62,
+				   0xf320ad74,
+				   0x8eb1c71e,
+				   0xbe8b0537,
+				   0xaa87ca22};
+static const uint32_t base_y[12]={0x90ea0e5f,
+				  0x7a431d7c,
+				  0x1d7e819d,
+				  0xa60b1ce,
+				  0xb5f0b8c0,
+				  0xe9da3113,
+				  0x289a147c,
+				  0xf8f41dbd,
+				  0x9292dc29,
+				  0x5d9e98bf,
+				  0x96262c6f,
+				  0x3617de4a};
 /* Arithmetic */
 typedef uint32_t felem[12];
 /*Debug function */
@@ -102,7 +136,6 @@ static void reduce_add_sub(felem r, uint32_t carry){
   }
   /* It should be the case that we only potentially need one subtraction. So if carry is set, should have pb set */
   /* Will do target tests (somehow: need to expose more internals. Or use test routines here)*/
-  assert((carry && pb) || !carry);
   do_sub = 1 - ((carry+pb)&0x01);
   mask_sub = (uint32_t) -do_sub; //assume 2's complement
   mask_nosub = ~ mask_sub;
@@ -123,21 +156,20 @@ static void add(felem r, const felem a, const felem b){
   reduce_add_sub(r, carry);
 }
 
-static void sub(felem r, const felem a, const felem b){
+static void sub(felem r, const felem a, const felem b){ /* Will need testing*/
   uint32_t carry = 0;
-  uint64_t t;
-  uint32_t brw;
-  uint32_t pbrw;
+  uint64_t t=0;
+  uint32_t brw=0;
+  uint32_t pbrw=0;
   for(int i=0; i<12; i++){ /* Assembler would be great for this.*/
     t = (uint64_t) prime[i]+a[i]+carry;
-    brw = t < b[i]+pbrw;
-    t += (uint64_t)brw<<32;
+    brw = t < ((uint64_t)b[i])+pbrw;
+    t += ((uint64_t)brw)<<32;
     t -= (uint64_t) b[i] + (uint64_t) pbrw;
     r[i] = (uint32_t) (t & mask_lo);
     carry = t >> 32;
     pbrw = brw;
   }
-  /* We know that there is no borrow here because we added p*/
   reduce_add_sub(r, carry);
 }
 
@@ -166,15 +198,21 @@ static void prod_red(felem r, uint32_t p[25]){ /* Broken why?*/
 }
 
 /* Question: how to multiply by 2, 3, and 8? A: 2 is easy, as is 3. For 8 multiply by 2 four times*/
-static void mul2( felem c, felem a){
+static void mul2( felem c, const felem a){
   add(c, a, a);
 }
-static void mul3(felem c, felem a){
+static void mul3(felem c, const felem a){
   mul2(c, a);
   add(c, c, a);
 }
 
-static void mul8(felem c, felem a){
+static void mul4(felem c, const felem a){
+  mul2(c, a);
+  mul2(c, c);
+
+}
+
+static void mul8(felem c, const felem a){
   felem t;
   mul2(c, a);
   mul2(t, c);
@@ -248,76 +286,390 @@ static void inv(felem r, const felem a){ /* Cannot be in place */
   }
   
 }
+static bool equal(const felem a, const felem b){ //Can leak *result*
+  uint32_t diff = 0;
+  for(int i=0; i<12; i++){
+    diff |= a[i] ^ b[i];
+  }
+  return diff == 0;
+}
 
+static bool iszero(const felem a){
+  uint32_t test = 0;
+  for(int i=0; i<12; i++){
+    test |= a[i];
+  }
+  return test ==0;
+}
+
+static void neg_conditional(felem r, const felem a, uint32_t cond){ /* Should try in-place*/
+  uint32_t diff[12];
+  uint32_t pbrw = 0;
+  uint32_t brw = 0;
+  uint64_t t;
+  uint32_t mask_diff;
+  uint32_t mask_nodiff;
+  for(int i=0; i<12; i++){
+    t = (uint64_t) prime[i];
+    brw = t < a[i]+pbrw;
+    t += (uint64_t) brw << 32;
+    t -= (uint64_t) a[i] + pbrw;
+    diff[i] = (uint32_t) (t & mask_lo); /* There is no carry because a<prime */
+  }
+  mask_diff = (uint32_t) -cond;
+  mask_nodiff = ~mask_diff;
+  for(int i=0; i<12; i++){
+    r[i] = (diff[i]&mask_diff) |(a[i] &mask_nodiff);
+  }
+}
+
+static void mov(felem r, const felem a){
+  for(int i=0; i<12; i++){
+    r[i]=a[i];
+  }
+}
+
+static void cmov(felem r, const felem a, uint32_t cond){
+  uint32_t mask_set;
+  uint32_t mask_unset;
+  mask_set = (uint32_t) -cond;
+  mask_unset = ~ mask_set;
+  for(int i=0; i<12; i++){
+    r[i] = (r[i]&mask_unset)|(a[i]&mask_set);
+  }
+}
+  
 /* Operations on the curve in Jacobian coordinates using readditions */
+/* We do not implement a specialized affine addition because inversion is slow (can fix) */
+/* The point at infinity is exceptional and leaks. To deal with this we use a signed form of exponents*/
 
+static bool oncurve(const felem x, const felem y, const felem z, bool not_mont){ //not montgomery
+  felem z2, z4, z6, t0, t1,t2, t3, t4, t5, ysqr, rhs, bmont, xm, ym, zm;
+  to_mont(bmont, curve_b);
+  if(not_mont){
+    to_mont(xm, x);
+    to_mont(ym, y);
+    to_mont(zm, z);
+  } else {
+    mov(xm, x);
+    mov(ym, y);
+    mov(zm, z);
+  }
+  mult(z2, zm, zm);
+  mult(z4, z2, z2);
+  mult(z6, z4, z2);
+  mult(t0, bmont, z6);
+  mult(t1, xm, z4);
+  mul3(t2, t1);
+  mult(t3, xm, xm);
+  mult(t4, t3, xm);
+  sub(t5, t4, t2);
+  add(rhs, t5, t0);
+  mult(ysqr, ym, ym);
+  return equal(ysqr, rhs);
+}
 
-/* Total operations for verification */
+static void double_pt(felem x3, felem y3, felem z3, const felem x1, const felem y1, const felem z1){
+  /* As the order is not divisible by 2, impossible to double finite point and get infinity */
+  /* Also works for point at infinity (assuming correct representation */
+  /* TODO: color */
+  /* From the EFD dbl-2001-b */
+  felem delta;
+  felem gamma;
+  felem beta;
+  felem alpha;
+  felem t0;
+  felem t1;
+  felem t2;
+  felem t3;
+  felem t4;
+  felem t5;
+  felem t6;
+  felem t7;
+  felem t8;
+  felem t9;
+  felem t10;
+  felem t11;
+  felem t12;
+  mult(delta, z1, z1); //specialize square after working
+  mult(gamma, y1, y1);
+  mult(beta, x1, gamma);
+  sub(t0, x1, delta);
+  add(t1, x1, delta);
+  mult(t2, t0, t1);
+  mul3(alpha, t2);
+  mult(t3, alpha, alpha);
+  mul8(t4, beta);
+  sub(x3, t3, t4);
+  add(t5, y1, z1);
+  mult(t6, t5,t5);
+  sub(t7, t6, gamma);
+  sub(z3, t7, delta);
+  mul4(t8, beta);
+  sub(t9, t8, x3);
+  mult(t10, gamma, gamma);
+  mul8(t11, t10);
+  mult(t12, alpha, t9);
+  sub(y3, t12, t11);
+}
 
+static void add_pt_tot(felem x3, felem y3, felem z3, const felem x1, const felem y1, const felem z1, const felem x2, const felem y2, const felem z2){
+  /* Special cases: z1 or z2 zero=> return the other point
+     if we are doubling: use the doubling.
+     if we produce infinity: set the output correctly */
+  /* Uses add-2007-bl. Note that test z1, z2, for pt at infinity (so return other one) and H for either double or inverse (return infinity)*/
+  felem z1z1;
+  felem z2z2;
+  felem u1;
+  felem u2;
+  felem t0;
+  felem t1;
+  felem t2;
+  felem s1;
+  felem s2;
+  felem h;
+  felem i;
+  felem j;
+  felem t3;
+  felem r;
+  felem v;
+  felem t4;
+  felem t5;
+  felem t6;
+  felem t7;
+  felem t8;
+  felem t9;
+  felem t10;
+  felem t11;
+  felem t12;
+  felem t13;
+  felem t14;
+  if(iszero(z1)){
+    mov(x3, x2);
+    mov(y3, y2);
+    mov(z3, z2);
+    return;
+  }else if(iszero(z2)){
+    mov(x3, x1);
+    mov(y3, y1);
+    mov(z3, z1);
+    return;
+  }
+  mult(z1z1, z1, z1);
+  mult(z2z2, z2, z2);
+  mult(u1, z2z2, x1);
+  mult(u2, z1z1, x2);
+  mult(t0, z2, z2z2);
+  mult(s1, y1, t0);
+  mult(t1, z1, z1z1);
+  mult(s2, y2, t1);
+  sub(h, u2, u1);
+  if(iszero(h)){
+    if(equal(s1,s2)){
+      double_pt(x3, y3, z3, x1, y1, z1);
+      return;
+    }else{
+      for(int k=0; k<12; k++){
+	x3[k]=0;
+	y3[k]=0;
+	z3[k]=0;
+      }
+      x3[0]=1;
+      y3[0]=1;
+      return;
+    }
+  }
+  mul2(t2, h);
+  mult(i, t2, t2);
+  mult(j, h, i);
+  sub(t2, s2, s1);
+  mul2(r, t3);
+  mult(v, u1, i);
+  mult(t4, r, r);
+  mul2(t5, v);
+  sub(t6, t4, j);
+  sub(x3, t6,t5);
+  sub(t7, v, x3);
+  mult(t8, s1, j);
+  mul2(t9, t8);
+  mult(t10, r, t7);
+  sub(y3, t10, t9);
+  add(t11, z1, z2);
+  mult(t12, t11, t11);
+  sub(t13, t12, z1z1);
+  sub(t14, t13, z2z2);
+  mult(z3, t14, h);
+}
+static void add_pt_const(felem x3, felem y3, felem z3, const felem x1, const felem y1, const felem z1, const felem x2, const felem y2, const felem z2){
+  /* Produces junk if used to add a point to itself or points at infinity. This is ok: we use flags and constant time moves*/
+  felem z1z1;
+  felem z2z2;
+  felem u1;
+  felem u2;
+  felem t0;
+  felem t1;
+  felem t2;
+  felem s1;
+  felem s2;
+  felem h;
+  felem i;
+  felem j;
+  felem t3;
+  felem r;
+  felem v;
+  felem t4;
+  felem t5;
+  felem t6;
+  felem t7;
+  felem t8;
+  felem t9;
+  felem t10;
+  felem t11;
+  felem t12;
+  felem t13;
+  felem t14;
+  mult(z1z1, z1, z1);
+  mult(z2z2, z2, z2);
+  mult(u1, z2z2, x1);
+  mult(u2, z1z1, x2);
+  mult(t0, z2, z2z2);
+  mult(s1, y1, t0);
+  mult(t1, z1, z1z1);
+  mult(s2, y2, t1);
+  sub(h, u2, u1);
+  mul2(t2, h);
+  mult(i, t2, t2);
+  mult(j, h, i);
+  sub(t3, s2, s1);
+  mul2(r, t3);
+  mult(v, u1, i);
+  mult(t4, r, r);
+  mul2(t5, v);
+  sub(t6, t4, j);
+  sub(x3, t6,t5);
+  sub(t7, v, x3);
+  mult(t8, s1, j);
+  mul2(t9, t8);
+  mult(t10, r, t7);
+  sub(y3, t10, t9);
+  add(t11, z1, z2);
+  mult(t12, t11, t11);
+  sub(t13, t12, z1z1);
+  sub(t14, t13, z2z2);
+  mult(z3, t14, h);
+}
+
+static void to_affine(felem x2, felem y2, const felem x1, const felem y1, const felem z1){
+  felem zr;
+  felem zrzr;
+  inv(zr, z1);
+  mult(zrzr, zr, zr);
+  mult(x2, x1, zrzr);
+  mult(zr, zrzr, zr);
+  mult(y2, y1, zr);
+}
+
+/*
+static void readd_comp;
+
+static void readd_pt */
 /* Table based multiplication routines */
+static void select_pt(felem x, felem y, felem z, felem table[][3], int index, int size){
+  for(int i=0; i<size; i++){
+    cmov(x, table[i][0], index==i);
+    cmov(y, table[i][1], index==i);
+    cmov(z, table[i][2], index==i);
+  }
+}
+
+static void scalarmult(felem x_out, felem y_out, const felem x_in, const felem y_in, unsigned char key[48]){
+  felem xm;
+  felem ym;
+  bool correct;
+  /*Note, we need to make sure that the key is < order for correctness */
+  /* For now double and add. Tables and signed NAF later */
+  /* Remember z==1, in montgomery form is R */
+  to_mont(xm, x_in);
+  to_mont(ym, y_in);
+  felem xQ, yQ, zQ;
+  felem xR, yR, zR;
+  for(int i=0; i<12; i++){
+    xQ[i]=0;
+    yQ[i]=0;
+    zQ[i]=0;
+  }
+  mov(xQ, R);
+  mov(yQ, R); //Q is pt at infinity
+  bool seen_1 = 0;
+  for(int i=47; i>=0; i--){ //little-endian
+    for(int j=7; j>=0; j--){
+      bool before_double = oncurve(xQ, yQ, zQ, false);
+      double_pt(xQ, yQ, zQ, xQ, yQ, zQ);
+      bool bit = (key[i]>>j) & 0x1;
+      add_pt_const(xR, yR, zR, xQ, yQ, zQ, xm, ym, R);
+      cmov(xR, xm, (1-seen_1));
+      cmov(yR, ym, (1-seen_1));
+      cmov(zR, R, (1-seen_1));
+      seen_1 = seen_1 | bit;
+      cmov(xQ, xR, bit);
+      cmov(yQ, yR, bit);
+      cmov(zQ, zR, bit);
+    }
+  }
+  to_affine(x_out, y_out, xQ, yQ, zQ);
+  from_mont(x_out, x_out);
+  from_mont(y_out, y_out);
+}
 
 /* Interface to outside world */
 
 /* Test routine: called main because I am lazy */
+/* TODO: KAT tests+others/real testing harness*/
 int main(){
-  felem a;
-  felem b;
-  felem c;
-  felem d;
-  felem e;
-  felem f;
-  felem t;
-  for(int i=0; i<12; i++){
-    a[i]=0;
-    b[i]=0;
-    c[i]=0;
-    d[i]=0;
-    e[i]=0;
-    f[i]=0;
-    t[i]=0;
+  felem Ax;
+  felem Ay;
+  felem Bx;
+  felem By;
+  felem Kax;
+  felem Kbx;
+  felem Kay;
+  felem Kby;
+  felem bxmont;
+  felem bymont;
+  unsigned char alicesk[48];
+  unsigned char bobsk[48];
+  for(int i=0; i<48; i++){
+    alicesk[i]=17;
+    bobsk[i]=19;
   }
-  printval("prime", prime);
   printval("R", R);
-  printf("print R == 2**384 %% prime\n");
-  printval("primes2", primes2);
-  printf("print prime-2 == primes2\n");
-  a[0]=15;
-  a[11]=47;
-  a[3]=5;
-  unsigned char out[48];
-  unsigned char out2[48];
-  to_mont(c, a);
-  from_mont(b, c);
-  pack(out, b);
-  pack(out2, a);
-  if(memcmp(out2, out, 48)){
-    printf("print montround: False \n");
+  printval("prime", prime);
+  printval("base_x", base_x);
+  printval("base_y", base_y);
+  printval("curve_b", curve_b);
+  printf("print \"pyparam:\", (base_y**2) %% prime == (base_x**3-3*base_x+curve_b) %% prime\n");
+  felem one = {1};
+  if(!oncurve(base_x, base_y, one, true)){
+    printf("print \"Params: \", False\n");
   }
-  mult(c, a, b);
-  printval("c_old", c);
-  printval("a_old", a);
-  printval("b_old", b);
-  printval("t_old", t);
-  printf("print c_old*R %% prime == a_old*b_old %% prime\n");
-  inv(d, b);
-  printval("a", a);
-  printval("b", b);
-  printval("c", c);
-  printval("t", t);
-  printf("print \"id c: \", c_old == c\n");
-  printf("print \"id b: \", b_old == b\n");
-  printf("print \"id a: \", a_old == a \n");
-  printf("print \"id t: \", t_old == t \n");
-  mult(e, b, d);
-  mult(f, c, d);
-  mult(t, a, R);
-  printval("d", d);
-  printval("e", e);
-  printval("f", f);
-  printval("t", t);
-  printf("print c*R %% prime == a*b %% prime\n");
-  printf("print e %% prime == R %% prime\n");
-  printf("print f*R %% prime == c*d %% prime\n");
-  printf("print f %% prime == a %% prime \n");
-  printf("print t %% prime == a %% prime \n");
+  to_mont(bxmont, base_x);
+  to_mont(bymont, base_y);
+  if(!oncurve(bxmont, bymont, R, false)){
+    printf("print \"Montparams:\", False \n");
+  }
+  scalarmult(Ax, Ay, base_x, base_y, alicesk);
+  if(!oncurve(Ax, Ay, one, true)){
+    printf("print \"Scalarmult alicesk failed\"\n");
+  }
+  scalarmult(Bx, By, base_x, base_y, bobsk);
+    if(!oncurve(Ax, Ay, one, true)){
+    printf("print \"Scalarmult bobsk failed\"\n");
+  }
+  scalarmult(Kax, Kay, Bx, By, alicesk);
+  scalarmult(Kbx, Kby, Ax, Ay, bobsk);
+  if(!equal(Kax, Kbx)){
+    printf("print \"DH:\", False\n");
+  } else {
+    printf("print \"DH:\", True\n");
+    }
 }
--- p384/print.py
+++ p384/print.py
@@ -9,9 +9,19 @@ def print32(x):
 prime = 2**384-2**128-2**96+2**32-1
 Rsqr = 2**(384*2)
 R = 2**384
+base_x = 0xaa87ca22be8b05378eb1c71ef320ad746e1d3b628ba79b9859f741e082542a385502f25dbf55296c3a545e3872760ab7
+base_y = 0x3617de4a96262c6f5d9e98bf9292dc29f8f41dbd289a147ce9da3113b5f0b8c00a60b1ce1d7e819d7a431d7c90ea0e5f
+curve_b = 27580193559959705877849011840389048093056905856361568521428707301988689241309860865136260764883745107765439761230575
 print "Rsqr"
 print32(Rsqr % prime)
 print "R"
 print32(R % prime)
 print "prime"
 print32(prime)
+print "curve_b"
+print32(curve_b)
+print "base_x"
+print32(base_x)
+print "base_y"
+print32(base_y)
+print "y^2+3x^2-b", (base_y**2-base_x**3+3*base_x-curve_b) % prime
